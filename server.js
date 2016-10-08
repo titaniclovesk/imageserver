643e8ef900a68a8fec13bf684a54cbbe99362bfe
SK Verma <titanic.love.sk@gmail.com>
2016-10-08 23:17:05 +0530

    fs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //This is image server
 var express = require("express");
 var app = express();
+var fs = require('fs');
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
